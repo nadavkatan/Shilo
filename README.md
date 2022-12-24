c56af0bd073d125e6b043c148f4b02d416aff539
nadavkatan <nadavkatan110@gmail.com>
2022-12-24 17:15:31 +0100

    change urls

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Shilo
-Shilo is a quizlet clone! It is a fullstack web application that was primarily developed with React, Nodejs, Express, and Mongoose. 
+
+Shilo is a quizlet clone! It is a fullstack web application that was primarily developed with React, Nodejs, Express, and Mongoose.
 Additional technologies implemented in Shilo are Material Ui, CKeditor (for text editing), unsplash api, and Passport js authentication.<br />
-**#Check out the live app at https://shilo-app.herokuapp.com/**
+**#Check out the live app at https://shilo.onrender.com/**
--- frontend/src/components/Context/utils/auth.js
+++ frontend/src/components/Context/utils/auth.js
@@ -1,48 +1,55 @@
-import axios from 'axios';
+import axios from "axios";
 
-export const checkAuth = async()=>{
-    const response = await axios.get('https://shilo-app.herokuapp.com/auth/check-auth', {withCredentials: true});
-    console.log(response)
-    const isAuth = response.data;
-    return isAuth;
-}
+export const checkAuth = async () => {
+  const response = await axios.get(
+    "https://shilo.onrender.com/auth/check-auth",
+    { withCredentials: true }
+  );
+  console.log(response);
+  const isAuth = response.data;
+  return isAuth;
+};
 
-export const login = async(credentials)=>{
-    const response = await axios({
-        method: 'post',
-        url: 'https://shilo-app.herokuapp.com/auth/login', 
-        data: credentials,
-        headers:{
-            "Content-Type": "application/json"
-        },
-        withCredentials: true
-    })
+export const login = async (credentials) => {
+  const response = await axios({
+    method: "post",
+    url: "https://shilo.onrender.com/auth/login",
+    data: credentials,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    withCredentials: true,
+  });
 
-    console.log(response.data)
-    const {isAuth} = response.data
-    if(!isAuth){
-       const {info} = response.data
-       return {isAuth, message: info.message}
+  console.log(response.data);
+  const { isAuth } = response.data;
+  if (!isAuth) {
+    const { info } = response.data;
+    return { isAuth, message: info.message };
+  }
+  return { isAuth };
+};
+
+export const logout = async () => {
+  // const response = await axios.delete('http://localhost:8000/auth/logout')
+  const response = await axios.get("https://shilo.onrender.com/auth/logout", {
+    withCredentials: true,
+  });
+  return response.data;
+};
+
+export const register = async (userData, cb) => {
+  try {
+    const newUser = await axios.post(
+      "https://shilo.onrender.com/auth/register",
+      userData
+    );
+    if (newUser.data === "User already exists") {
+      return cb(newUser.data);
+    } else {
+      return "success";
     }
-    return {isAuth}
-}
-
-export const logout = async()=>{
-    // const response = await axios.delete('http://localhost:8000/auth/logout')
-    const response = await axios.get('https://shilo-app.herokuapp.com/auth/logout', {withCredentials: true})
-    return response.data;
-}
-
-export const register = async(userData, cb)=>{
-    try{
-        const newUser = await axios.post('https://shilo-app.herokuapp.com/auth/register', userData)
-        if(newUser.data === "User already exists"){
-           return cb(newUser.data);
-        }else{
-            return "success"
-        }
-    }catch(e){
-        console.log(e)
-    }
-
-}
+  } catch (e) {
+    console.log(e);
+  }
+};
